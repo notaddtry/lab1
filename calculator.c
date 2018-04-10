25a7a08779468d743867520372abd6ae9424b541
notaddtry <33911781+notaddtry@users.noreply.github.com>
2018-04-11 00:24:59 +0300

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h> 
+ int atoi (const char *str);
+ int main()
+ {
+     int stack[1000];
+     char buf[1000];
+     int sp =0;
+    printf("Input Enter numbers and characters in the write-back\n");
+     while (!feof(stdin)) {
+        if(scanf ("%s", buf) != 1 )
+            break;
+        switch(buf[0])
+        {
+            case '\0':
+                break;
+            case '=':
+                 printf("Result = %d\n", stack[sp - 1]);  sp--;
+                 break;
+             case '+':
+                stack[sp-2] = stack[sp-2] + stack[sp-1];  sp--;
+                break;
+             case '-':
+                stack[sp-2] = stack[sp-2] - stack[sp-1];  sp--;
+                break;
+             case '*':
+                stack[sp-2] = stack[sp-1] * stack[sp-2];  sp--;
+                break;
+             case '/':
+               stack[sp-2] = stack[sp-2] / stack[sp-1];   sp--;
+                break;
+             default:
+                 stack[sp++] = atoi(buf);
+        }
+    }
+     printf("Result = %d\n",stack[sp-1]);
+     return 0;
+ }
